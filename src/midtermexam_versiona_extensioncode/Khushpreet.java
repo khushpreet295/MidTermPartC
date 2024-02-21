076344eb7ebb719cc964f35d6a253dc262189f4b
khush <khush@10.48.52.187>
2024-02-21 14:22:08 -0500

    second commit for midterm(adding personal comments)

---
+++ src/midtermexam_versiona_extensioncode/Khushpreet.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author khush
+ */
+public class Khushpreet {
+    /*
+    My name is khushpreet singh;
+    currently im giving my Midterm exam of subject software development
+    current time is 2:21
+    date is 21 feb, 2024
+    
+    */
+}
